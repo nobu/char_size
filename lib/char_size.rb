4c426cdd5aea9ad08e4abb85e2732a034d12f913
Andrew Haines <andrew@haines.org.nz>
2017-05-20 21:01:53 +0100

    Initial commit

---
+++ lib/char_size.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require "char_size/version"
+
+module CharSize
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+
+require "simplecov"
+SimpleCov.start do
+  add_filter "/test/"
+end
+
+require "minitest/autorun"
