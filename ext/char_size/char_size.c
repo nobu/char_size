e7ca2d60993d4408be5e1628b63f89a2913fc1b8
Andrew Haines <andrew@haines.org.nz>
2017-05-23 06:09:57 +0100

    Match Ruby core indentation style

--- ext/char_size/char_size.c
+++ ext/char_size/char_size.c
@@ -8,8 +8,9 @@
  *   CharSize.min("UTF-8")         # => 1
  *   CharSize.min(Encoding::UTF_8) # => 1
  */
-static VALUE min(VALUE class, VALUE encoding_or_name) {
-  return INT2NUM(ONIGENC_MBC_MINLEN(rb_to_encoding(encoding_or_name)));
+static VALUE min(VALUE class, VALUE encoding_or_name)
+{
+    return INT2NUM(ONIGENC_MBC_MINLEN(rb_to_encoding(encoding_or_name)));
 }
 
 /*
@@ -20,12 +21,14 @@ static VALUE min(VALUE class, VALUE encoding_or_name) {
  *   CharSize.max("UTF-8")         # => 6
  *   CharSize.max(Encoding::UTF_8) # => 6
  */
-static VALUE max(VALUE class, VALUE encoding_or_name) {
-  return INT2NUM(ONIGENC_MBC_MAXLEN(rb_to_encoding(encoding_or_name)));
+static VALUE max(VALUE class, VALUE encoding_or_name)
+{
+    return INT2NUM(ONIGENC_MBC_MAXLEN(rb_to_encoding(encoding_or_name)));
 }
 
-void Init_char_size() {
-  VALUE CharSize = rb_define_module("CharSize");
-  rb_define_singleton_method(CharSize, "min", min, 1);
-  rb_define_singleton_method(CharSize, "max", max, 1);
+void Init_char_size()
+{
+    VALUE CharSize = rb_define_module("CharSize");
+    rb_define_singleton_method(CharSize, "min", min, 1);
+    rb_define_singleton_method(CharSize, "max", max, 1);
 }
