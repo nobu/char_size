85a2b9f8c35f9fc122ceeb3f228b1a7c6444165b
Nobuyoshi Nakada <nobu@ruby-lang.org>
2017-05-23 12:31:07 +0900

    Move `minmax` to the extension

--- ext/char_size/char_size.c
+++ ext/char_size/char_size.c
@@ -26,8 +26,28 @@ static VALUE max(VALUE class, VALUE encoding_or_name) {
   return INT2NUM(ONIGENC_MBC_MAXLEN(enc));
 }
 
+/*
+ * Gets the minimum and maximum character size (in bytes) for an encoding.
+ * @param encoding_or_name [Encoding, String] the encoding or its name
+ * @return [Array<(Integer, Integer)>] the minimum and maximum character size
+ * @example
+ *   CharSize.minmax("UTF-8")         # => [1, 6]
+ *   CharSize.minmax(Encoding::UTF_8) # => [1, 6]
+ */
+static VALUE minmax(VALUE class, VALUE encoding_or_name) {
+  rb_encoding *enc = rb_to_encoding(encoding_or_name);
+  VALUE min = INT2NUM(ONIGENC_MBC_MINLEN(enc));
+  VALUE max = INT2NUM(ONIGENC_MBC_MAXLEN(enc));
+  return rb_assoc_new(min, max);
+}
+
+/*
+ * The +CharSize+ module provides functions that return the character size limits
+ * of encodings.
+ */
 void Init_char_size() {
   VALUE CharSize = rb_define_module("CharSize");
   rb_define_singleton_method(CharSize, "min", min, 1);
   rb_define_singleton_method(CharSize, "max", max, 1);
+  rb_define_singleton_method(CharSize, "minmax", minmax, 1);
 }
--- lib/char_size.rb
+++ lib/char_size.rb
@@ -2,17 +2,3 @@
 
 require "char_size/char_size"
 require "char_size/version"
-
-# The +CharSize+ module provides functions that return the character size limits
-# of encodings.
-module CharSize
-  # Gets the minimum and maximum character size (in bytes) for an encoding.
-  # @param encoding_or_name [Encoding, String] the encoding or its name
-  # @return [Array<(Integer, Integer)>] the minimum and maximum character size
-  # @example
-  #   CharSize.minmax("UTF-8")         # => [1, 6]
-  #   CharSize.minmax(Encoding::UTF_8) # => [1, 6]
-  def self.minmax(encoding_or_name)
-    [min(encoding_or_name), max(encoding_or_name)]
-  end
-end
